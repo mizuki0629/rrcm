4d972e35c39c1bae366b00dd18bf33ca710055c3
mizuki0629 <mizuki0629@gmail.com>
2023-07-14 23:03:33 +0900

    add subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use anyhow;
+use clap::{Parser, Subcommand};
+use color_print::cprintln;
 use dirs;
+use itertools::Itertools;
+use std::collections::BTreeSet;
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::ffi::OsString;
 use std::fs;
 use std::fs::DirEntry;
 use std::path;
 use std::path::Path;
+use std::path::PathBuf;
+
 // 存在するか       Y Y Y N
 // Link             Y Y N /
 // Link先が正しいか Y N / /
@@ -21,15 +26,15 @@ fn create_filemap(path: &path::Path) -> anyhow::Result<HashMap<OsString, DirEntr
     anyhow::Ok(dst)
 }
 
-#[derive(Debug,PartialEq,Eq)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 enum Status {
-    Managed,
+    UnDeployed,
     Deployed,
     Conflict,
     UnManaged,
 }
 
-fn status(src: Option<&DirEntry>, dst: Option<&DirEntry>) -> Status {
+fn get_status(src: Option<&DirEntry>, dst: Option<&DirEntry>) -> Status {
     let mut s = Status::UnManaged;
     if let Some(src_entry) = src {
         if let Some(dst_entry) = dst {
@@ -42,58 +47,254 @@ fn status(src: Option<&DirEntry>, dst: Option<&DirEntry>) -> Status {
                 }
             }
         } else {
-            s = Status::Managed;
+            s = Status::UnDeployed;
         }
     }
     s
 }
 
-fn manage(src: &Path, dst_dir: &Path) -> anyhow::Result<()> {
-    let dst = Path::new(dst_dir).join(src.file_name().unwrap());
-    std::fs::rename(src, dst)?;
-    Ok(())
+#[cfg(target_family = "unix")]
+fn symlink<P, Q>(from: P, to: Q) -> anyhow::Result<()>
+where
+    P: AsRef<Path>,
+    Q: AsRef<Path>,
+{
+    std::os::unix::fs::symlink(from, to)?;
+    return Ok(());
 }
 
-#[cfg(target_family = "unix")]
-fn deploy(src: &Path, dst_dir: &Path) -> anyhow::Result<()> {
-    let dst = Path::new(dst_dir).join(src.file_name().unwrap());
-    std::os::unix::fs::symlink(src, dst)?;
-    Ok(())
+fn deploy<P>(path: P, targets: Vec<Target>) -> anyhow::Result<()>
+where
+    P: AsRef<Path>,
+{
+    let path = path.as_ref();
+    let canonicalized_path = path.canonicalize()?;
+    for target in targets {
+        if target.from == canonicalized_path.parent().unwrap() {
+            std::os::unix::fs::symlink(path, target.to.join(path.file_name().unwrap()))?;
+            return Ok(());
+        }
+    }
+    return Err(anyhow::anyhow!(
+        "{:?} is not managed directory. Please add config.",
+        canonicalized_path
+    ));
+}
+
+fn add<P>(path: P, targets: Vec<Target>) -> anyhow::Result<()>
+where
+    P: AsRef<Path>,
+{
+    let path = path.as_ref();
+    let canonicalized_path = path.canonicalize()?;
+    for target in targets {
+        if target.to == canonicalized_path.parent().unwrap() {
+            let manage_path = target.from.join(path.file_name().unwrap());
+            if manage_path.exists() {
+                return Err(anyhow::anyhow!("{:?} is already exists.", manage_path));
+            }
+            std::fs::rename(path, manage_path.clone())?;
+            symlink(manage_path, path)?;
+            return Ok(());
+        }
+    }
+    return Err(anyhow::anyhow!(
+        "{:?} is not managed directory. Please add config.",
+        canonicalized_path
+    ));
+}
+
+struct Target {
+    from: PathBuf,
+    to: PathBuf,
+}
+
+fn get_targets() -> Vec<Target> {
+    let mut targets = Vec::new();
+
+    let mut from = dirs::home_dir().unwrap();
+    from.push("dotfiles");
+    from.push("config");
+
+    targets.push(Target {
+        from,
+        to: dirs::config_dir().unwrap(),
+    });
+
+    let mut from = dirs::home_dir().unwrap();
+    from.push("dotfiles");
+    from.push("home");
+    targets.push(Target {
+        from,
+        to: dirs::home_dir().unwrap(),
+    });
+
+    targets
+}
+
+fn list() -> anyhow::Result<()> {
+    println!("Status From To");
+    for target in get_targets() {
+        let from_files = create_filemap(&target.from)?;
+        let to_files = create_filemap(&target.to)?;
+
+        let filelist = from_files
+            .keys()
+            .chain(to_files.keys())
+            .collect::<BTreeSet<_>>();
+
+        println!("-- {:?} {:?}", target.from, target.to);
+        for key in filelist {
+            let from = from_files.get(key);
+            let to = to_files.get(key);
+            let status = get_status(from, to);
+
+            println!(
+                "{:?} {:?} {:?}",
+                status,
+                from.map_or(OsString::new(), |v| v.file_name()),
+                to.map_or(OsString::new(), |v| v.file_name()),
+            );
+        }
+    }
+    anyhow::Ok(())
+}
+
+fn print_status(
+    path: &Path,
+    lookup: &HashMap<Status, Vec<(Option<&DirEntry>, Option<&DirEntry>)>>,
+    s: Status,
+) -> anyhow::Result<()> {
+    if let Some(l) = lookup.get(&s) {
+        println!("");
+        match s {
+            Status::Deployed => {
+            },
+            Status::UnDeployed => {
+                println!("Files are not deployed:");
+                println!("  (use \"rrcm deploy <file>...\" to deploy files)");
+            }
+            Status::UnManaged => {
+                println!("  (use \"rrcm add <file>...\" to manage and deploy files)");
+            },
+            Status::Conflict => {
+                println!("  (symlink is not to managed file or not symlink.)");
+                println!("  (you can move or delete file or )");
+                println!("  (use \"rrcm deploy -f <file>...\" force deploy files)");
+            },
+        }
+
+        for f in l {
+            let ff = match s {
+                Status::Deployed => f.1,
+                Status::UnDeployed => f.0,
+                Status::UnManaged => f.1,
+                Status::Conflict => f.1,
+            }
+            .unwrap()
+            .path();
+            let ff = ff.strip_prefix(path).unwrap_or(ff.as_path()).to_str().unwrap();
+            match s {
+                Status::Deployed => cprintln!("    <green>{:?}: {:}</>", s, ff),
+                Status::UnDeployed => cprintln!("    <yellow>{:?}: {:}</>", s, ff),
+                Status::UnManaged => cprintln!("    <dim>{:?}: {:}</>", s, ff),
+                Status::Conflict => cprintln!("    <red>{:?}: {:}</>", s, ff),
+            };
+        }
+    }
+
+    return Ok(());
+}
+
+fn status<P>(path: P, targets: Vec<Target>) -> anyhow::Result<()>
+where
+    P: AsRef<Path>,
+{
+    let path = path.as_ref();
+    let canonicalized_path = path.canonicalize()?;
+    for target in targets {
+        if target.to == canonicalized_path {
+            let from_files = create_filemap(&target.from)?;
+            let to_files = create_filemap(&target.to)?;
+
+            let filelist = from_files
+                .keys()
+                .chain(to_files.keys())
+                .collect::<BTreeSet<_>>();
+
+            let lookup = filelist
+                .iter()
+                .map(|f| {
+                    let from = from_files.get(*f);
+                    let to = to_files.get(*f);
+                    (get_status(from, to), (from, to))
+                })
+                .into_group_map();
+
+            print_status(path, &lookup, Status::Deployed)?;
+            print_status(path, &lookup, Status::Conflict)?;
+            print_status(path, &lookup, Status::UnManaged)?;
+            print_status(path, &lookup, Status::UnDeployed)?;
+
+            return Ok(());
+        }
+    }
+    return Err(anyhow::anyhow!(
+        "{:?} is not managed directory.",
+        canonicalized_path
+    ));
+}
+
+#[derive(Debug, Parser)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    arg_required_else_help = true,
+)]
+struct Args {
+    #[clap(subcommand)]
+    subcommand: SubCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum SubCommands {
+    Status {
+        #[clap(default_value = ".")]
+        path: PathBuf,
+    },
+    /// Print
+    List,
+    /// Add
+    Add {
+        /// file or directory path
+        #[clap(required = true, ignore_case = true)]
+        path: PathBuf,
+    },
+    /// Deploy config file
+    Deploy,
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut path = dirs::home_dir().unwrap();
-    path.push("dotfiles");
-    path.push("config");
+    let args = Args::parse();
 
-    let dotfiles = create_filemap(&path)?;
-
-    let config_dir_files = create_filemap(&dirs::config_dir().unwrap())?;
-
-    let a = dotfiles.keys().collect::<HashSet<_>>();
-    let b = config_dir_files.keys().collect::<HashSet<_>>();
-    let filelist = a.union(&b).collect::<Vec<_>>();
-
-    println!("config");
-    for key in filelist {
-        let src = dotfiles.get(*key);
-        let dst = config_dir_files.get(*key);
-        let s = status(src, dst);
-        println!(
-            "{:?} {:?} {:?}",
-            s,
-            if let Some(entry) = src {
-                entry.file_name()
-            } else {
-                OsString::new()
-            },
-            if let Some(entry) = dst {
-                entry.file_name()
-            } else {
-                OsString::new()
-            },
-        );
+    match args.subcommand {
+        SubCommands::Status { path } => {
+            let targets = get_targets();
+            status(path.as_path(), targets)
+        }
+        SubCommands::List => list(),
+        SubCommands::Add { path } => {
+            let targets = get_targets();
+            add(path.as_path(), targets)
+        }
+        SubCommands::Deploy => {
+            // let targets = get_targets();
+            // let path = Path::new("/home/mizuki/dotfiles/config/rrcm");
+            // deploy(path, targets)?;
+            println!("{:?}", args.subcommand);
+            anyhow::Ok(())
+        }
     }
-
-    anyhow::Ok(())
 }
